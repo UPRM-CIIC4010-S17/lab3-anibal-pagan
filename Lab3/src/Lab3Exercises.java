b29871a2382908f83463702daa9e7f54d411d7d0
4015c13 <4015c13@nispero.ece.uprm.edu>
2017-11-15 16:34:44 -0400

    Added two borders and a background

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -8,5 +8,7 @@ public class Lab3Exercises {
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 		
+	     MyPanelClass myPanel = new MyPanelClass();
+         myFrame.getContentPane().add(myPanel);
 	}
 }
